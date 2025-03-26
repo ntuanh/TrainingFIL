94e17ed972aeaf3313e6960ea3ea0f9b37feb204
ntuanh <ntuanh2705@gmail.com>
2025-03-26 02:10:56 +0000

    [UPDATE] using K and G - binary

--- PythonandSocket/Python/GeneralConvolutionalEncoder.py
+++ PythonandSocket/Python/GeneralConvolutionalEncoder.py
@@ -1,46 +1,30 @@
-node = [[] for _ in range(6)]
-OUTPUT = [3, 5]
-class ConvolutionalEncoder :
-    def __init__(self , chars):
-        # setup by hand
-        self.chars = chars
-        node[1] = ['A', 'B']
-        node[2] = ['1', 'C']
-        node[3] = ['2', 'D']
-        node[4] = ['A', 'C']
-        node[5] = ['4', 'D']
+#setup
+K = 3
+G = ['111', '101']
 
-    def display(self):
-        print("Hello worlddd")
-        print(self.chars[get_indice('A')])
+
+class ConvolutionalEncoder:
+    def __init__(self , input_bits):
+        # setup by hand
+        self.input_bits = input_bits
 
     def compute(self):
-        com_node = [0]*6
-        for i in range(1 , 6 , 1):
-            print("node" , i)
-            for char in node[i]:
-                if char >= 'A' and char <= 'Z' :
-                    print(f"Before res is  {com_node[i]} and {int(self.chars[get_indice(char)])}")
-                    com_node[i] = XOR(com_node[i], int(self.chars[get_indice(char)]))
-                    print(f"Computed {com_node[i]}")
-                else:
-                    com_node[i] = XOR(com_node[int(char)] , com_node[i])
-        print("RESULT")
-        for node_i in OUTPUT:
-            print(com_node[node_i] , end="")
+        for gen in G :
+            res = 0
+            for i in range(len(gen)):
+                if gen[i] == '1':
+                    res = XOR(res , int(self.input_bits[i]))
+            print(res , end='')
 
 
-def get_indice(x):
-    return int(ord(x) - ord('A'))
+def compute_bits(bits):
+    bits = '0'*(K - 1) + bits
+    for i in range(len(bits) - K + 1):
+        ConvolutionalEncoder(bits[i : i + K][::-1]).compute()
 
 def XOR(a , b):
-    if a == b:
-        return 0
-    else :
-        return 1
+    return 0 if a == b else 1
 
-encoder = ConvolutionalEncoder("1001")
-encoder.display()
-encoder.compute()
+compute_bits('1100')
 
 
